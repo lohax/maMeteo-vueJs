f70809daf309631e55734f171d046b2969103e05
Loic Chambost <chambost.loic@gmail.com>
2021-01-25 17:33:40 +0100

    ready build

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mameteo/'
+    : '/',
+  pwa: {
+    name: 'Ma Météo',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW'
+  }
+}
